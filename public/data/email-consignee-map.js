1cee1b5da39232f2cc6cebddd349440be4f622fc
jonelpechfruits-cpu <jonelpechfruits@gmail.com>
2025-11-13 23:41:30 +0200

    Update email-consignee-map.js

--- public/data/email-consignee-map.js
+++ public/data/email-consignee-map.js
@@ -3,6 +3,6 @@ window.EMAIL_CONSIGNEE_MAP = {
   "newvisionfruitsuarl@gmail.com": "NEW VISION",
   "accounts.za@pechfruits.com": "ALL",
   "southafrica@pechfruits.com": "ALL",
-  "test@pechfruits.com": "NEW VISION"  // optional test
+  "test@pechfruits.com": "NEW VISION",  // optional test
   "allfoodsserviceslc23@yahoo.com": "ALLFOODS"
 };
